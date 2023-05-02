d28470eb8b1309a13ec14ce3a04830a94f7588a6
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-05-02 14:27:35 +0800

    pending returning create customer

--- src/services/stripeCustomer.service.ts
+++ src/services/stripeCustomer.service.ts
@@ -1,5 +1,18 @@
+import Stripe from "stripe";
 
+const stripe = new Stripe(
+  'sk_test_51N1mlOC8dPvRUIH159PGixF39AEuGX74MWaGOhQHdq1trCSC8vBHdJe9trI04lX1paI8EtQC3tykEvXKk7RxRpdX00XVLFc4q2', 
+  { apiVersion: '2022-11-15' }
+  );
 
-export async function create() {
+export async function createCustomer() {
+  const params: Stripe.CustomerCreateParams = {
+    email: 'khalid2@test.com',
+  };
 
+  const customer: Stripe.Customer = await stripe.customers.create(params);
+
+  return {
+    message: "Successfully create customer"
+  }
 }
