b70facb3d2fbbb8512d7f9313c760ab108a312e2
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-05-03 16:10:41 +0800

    wokring on create checkout session

--- src/routers/stripe.router.ts
+++ src/routers/stripe.router.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import { StripeCustomerController } from '../controllers/StripeCustomer.controller';
+import { StripeCheckoutSessionController } from '../controllers/StripeCheckoutSession.controller';
 
 const router = express.Router();
 const stripeCustomerController = new StripeCustomerController()
+const stripeCheckoutSessionController = new StripeCheckoutSessionController()
 
 // Customer
 router.post('/create-customer', stripeCustomerController.create)
+
+// Checkout Session
+router.post('/create-checkout-session', stripeCheckoutSessionController.create)
 
 export default router;
