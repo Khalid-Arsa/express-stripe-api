a324d83614f976d1101c876bd675698f570fb45d
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-05-08 09:38:51 +0800

    pending test stripe checkout session issue

--- src/__test__/StripeCheckoutSession.test.ts
+++ src/__test__/StripeCheckoutSession.test.ts
@@ -4,13 +4,6 @@ import { createStripeCheckoutSessionInput } from "./lib/StripeCheckoutSession.te
 import { server } from "../server"
 
 describe("POST api/stripe/create-checkout-session", () => {
-  afterEach(async () => {
-    server.close()
-    await new Promise<void>((resolve) => {
-      setTimeout(() => resolve(), 500);
-    }); // avoid jest open handle error when testing on db
-  })
-
   test("CREATE CHECKOUT SESSION: It should satify the correct payload", async () => {
     const response = await request(server)
       .post(endPoints.STRIPE_CHECKOUT_SESSION_API)
