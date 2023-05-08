67c7702c718d886a5b02a9adb048da8d96c8ecd3
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-05-08 16:12:31 +0800

    working test checkout session

--- src/services/StripeCheckoutSession.service.ts
+++ src/services/StripeCheckoutSession.service.ts
@@ -6,39 +6,42 @@ const stripe = new Stripe(config.secret as string, {
   apiVersion: "2022-11-15",
 });
 
-const succ_url = "http://localhost:1338/api/stripe/create-checkout-session/success";
-const can_url = "http://localhost:1338/api/stripe/create-checkout-session/cancel";
+const succ_url =
+  "http://localhost:1338/api/stripe/create-checkout-session/success";
+const can_url =
+  "http://localhost:1338/api/stripe/create-checkout-session/cancel";
 
 export async function stripeCreateCheckoutSession(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const { product } = req.body;
+  const { name, price, quantity } = req.body;
   const params: Stripe.Checkout.SessionCreateParams = {
     success_url: succ_url,
-    cancel_url: can_url, 
+    cancel_url: can_url,
     mode: "payment",
     line_items: [
       {
         price_data: {
-          currency: "php",
+          currency: "usd",
           product_data: {
-            name: product.name,
+            name: name || "None Product",
           },
-          unit_amount: product.price * 100,
+          unit_amount: price || 2000,
         },
-        quantity: product.quantity,
+        quantity: quantity || 1,
       },
     ],
-    payment_method_types: ["card"]
-  }
+    payment_method_types: ["card"],
+  };
 
-  const checkout: Stripe.Checkout.Session = await stripe.checkout.sessions.create(params);
+  const checkout: Stripe.Response<Stripe.Checkout.Session> =
+    await stripe.checkout.sessions.create(params);
 
   return res.status(200).json({
     success: true,
     message: "Successfully create checkout",
-    checkoutId: checkout.id
-  })
+    checkoutId: checkout.id,
+  });
 }
