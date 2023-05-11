c3ab39487e313b8a33a3d7063f8499a59db4a21c
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-05-11 14:17:55 +0800

    testing for retrieve customer stripe

--- src/__test__/lib/test.lib.ts
+++ src/__test__/lib/test.lib.ts
@@ -1,6 +1,7 @@
 export const headers = { APPLICATION_JSON: 'application/json' };
 
 export const endPoints = {
-  STRIPE_CUSTOMER_API: '/api/stripe/create-customer',
+  STRIPE_CREATE_CUSTOMER_API: '/api/stripe/create-customer',
+  STRIPE_GET_CUSTOMER_API: '/api/stripe/retrieve-customer/cus_Npx2r3XFmG62gm',
   STRIPE_CHECKOUT_SESSION_API: '/api/stripe/create-checkout-session'
 };
