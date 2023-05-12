c8c9d3941230f89227429b8a22b946e7554aaed4
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-05-12 14:03:20 +0800

    working on get all customer stripe

--- src/__test__/StripeCustomer.test.ts
+++ src/__test__/StripeCustomer.test.ts
@@ -1,23 +1,8 @@
 import supertest from "supertest";
 import { server } from "../server";
-import { endPoints, headers } from "./lib/test.lib";
-import { createStripeCustomerInput } from "./lib/StripeCustomer.test.lib";
+import { endPoints } from "./lib/test.lib";
 
 describe("POST api/stripe/create-customer", () => {
-  test("CREATE CUSTOMER: It should satify the correct payload", async () => {
-    const response = await supertest(server)
-      .post(endPoints.STRIPE_CREATE_CUSTOMER_API)
-      .send(createStripeCustomerInput)
-      .set("Accept", headers.APPLICATION_JSON);
-
-    const { success, message, customerId } = response.body;
-
-    expect(response.statusCode).toBe(200);
-    expect(typeof message === "string").toBe(true);
-    expect(typeof customerId === "string").toBe(true);
-    expect(success).toBe(true);
-  });
-
   test("RETRIEVE CUSTOMER: It should return correct data", async () => {
     const response = await supertest(server).get(
       endPoints.STRIPE_GET_CUSTOMER_API
--- src/routers/stripe.router.ts
+++ src/routers/stripe.router.ts
@@ -9,6 +9,7 @@ const stripeCheckoutSessionController = new StripeCheckoutSessionController()
 // Customer
 router.post('/create-customer', stripeCustomerController.create)
 router.get('/retrieve-customer/:id', stripeCustomerController.get)
+router.get('/retrieve-customers', stripeCustomerController.getAll)
 
 // Checkout Session
 router.post('/create-checkout-session', stripeCheckoutSessionController.create)
